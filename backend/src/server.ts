6f1403b96f90cdc5d98a34a680e0db0dff341abb
vivekpvk.01 <pinipevivek01@gmail.com>
2026-03-01 18:46:47 +0530

    compatible server.js

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ async function startServer() {
 
     const app = createApp()
 
-    const PORT = process.env.PORT || 5000
+    const PORT = Number(process.env.PORT) || 5000
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server running on port ${PORT}`)
